f5b64c35233550cdb816fcda1ad2d54ad545a7ec
tiago-py <tiago.braga0012@gmail.com>
2023-02-13 15:25:47 -0300

    aula2

---
+++ codigo2.php
@@ -0,0 +1,4 @@
+<?php
+    $idade = readline('Digite sua idade: ');
+    echo "você tem $idade anos \n";
+    # aspas simples = texto puro, aspas duplas = texto concatenado ou seja pode misturar ate com a mãe
