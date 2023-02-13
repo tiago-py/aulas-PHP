5655ae2073d2162874bb38855a2531cfc2589aac
tiago-py <tiago.braga0012@gmail.com>
2023-02-13 16:32:40 -0300

    aula2

---
+++ codigo4.php
@@ -0,0 +1,9 @@
+<?php
+    $nome = "Ricardo santos";
+    $cidade = "Mogi Mirim";
+    $estado = "SP";
+    $junta = "$cidade $estado";
+    echo "$junta \n";
+    $num = 10;
+    $valor = "15 bolas";
+    echo $num . $valor;
