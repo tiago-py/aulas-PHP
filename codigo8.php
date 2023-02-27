f14fd4cb524bc7707cc35092a822f859a27e6e66
tiago-py <108705110+tiago-py@users.noreply.github.com>
2023-02-27 16:26:52 -0300

    Create codigo8.php

---
+++ codigo8.php
@@ -0,0 +1,13 @@
+<?php 
+
+    #Arrays --> vetores
+
+    $impares = [1, 3, 5, 7, 9];
+    $pares = [0, 2, 4, 6, 8];
+    echo $impares[2];
+    echo "\n";
+    echo $pares[4]; 
+    echo "\n";
+    print_r($impares);
+    echo "\n";
+    print_r($pares);
