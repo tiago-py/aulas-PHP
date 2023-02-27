80a51bb61a42a97aa2d997ada4865c82cf327856
tiago-py <108705110+tiago-py@users.noreply.github.com>
2023-02-27 16:03:48 -0300

    Create codigo7.php

---
+++ codigo7.php
@@ -0,0 +1,11 @@
+
+<?php 
+
+    $num = 6;
+    $texto = "6";
+    $outra = "$num vezes campeão brasileiro \n";
+    echo $outra;
+    echo $num + $texto;
+    echo "\n";
+    echo $texto * $outra;
+    
