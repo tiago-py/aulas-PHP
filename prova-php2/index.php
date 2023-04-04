0238033bd3b49d9c7d08e9e0cbc320b0aaacad87
tiago-py <tiago.braga0012@gmail.com>
2023-04-03 21:46:43 -0300

    prova

--- prova-php2/index.php
+++ prova-php2/index.php
@@ -1,23 +1,29 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro</title>
 </head>
-
 <body>
-    <form action="codigo.php" method="post">
-        <input type="text" name='nome'><br>
-        <input type="email" name='email'><br>
-        <input type="text" name='endereco'><br>
-        <input type="text" name='turma'><br>
-        <input type="number" name='idade'><br>
-        <button type="submit"value="Enviar">Cadastrar</button>
+	<h1>Cadastro</h1>
+	<form method="post" action="codigo.php" target="_blank">
+		<label>Nome:</label><br>
+		<input type="text" name="nome"><br>
+		<label>E-mail:</label><br>
+		<input type="email" name="email"><br>
+		<label>Enderesço:</label><br>
+		<input type="text" name="endereco"><br>
+		<label>Turma:</label><br>
+		<input type="text" name="turma"><br>
+		<label>Idade:</label><br>
+		<input type="number" name="idade"><br><br>
+		<input type="submit" value="Cadastrar">
+	</form>
+</body>
+</html>
 
-    </form>
 </body>
 
 </html>
